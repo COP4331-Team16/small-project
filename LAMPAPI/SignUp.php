649a47a82af46084900efb48b12ee1268fad233c
Cam <150202195+CameronLewis08@users.noreply.github.com>
2026-01-29 23:40:55 -0500

    Create SignUp.php
    Used Login api to model a sign up that takes a users first name , last name, login, and password then checks if the they are all valid inputs and sees if the user is currently taken if not it will create a new user in the database and return a json with the first and last name as well as the unique user id.

---
+++ LAMPAPI/SignUp.php
@@ -0,0 +1,90 @@
+
+<?php
+
+	header('Content-Type: application/json');
+	$inData = getRequestInfo();
+    
+	$id = 0;
+	$firstName = "";
+	$lastName = "";
+
+	// needs to be updated with correct database info!!!!!
+
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");     
+
+	if( $conn->connect_error )
+	{
+		returnWithError( $conn->connect_error );
+	}
+	else
+	{
+		// Basic input validation
+		$firstName = trim($inData['firstName'] ?? '');
+		$lastName = trim($inData['lastName'] ?? '');
+		$login = trim($inData['login'] ?? '');
+		$password = $inData['password'] ?? '';
+
+		if($firstName === '' || $lastName === '' || $login === '' || $password === '') {
+			returnWithError("Missing required fields");
+			$conn->close();
+			exit;
+		}
+
+		// Check if user already exists
+		$stmt = $conn->prepare("SELECT ID FROM Users WHERE Login = ?");
+		$stmt->bind_param("s", $login);
+		$stmt->execute();
+		$result = $stmt->get_result();
+
+		if( $result->num_rows > 0 )
+		{
+			$stmt->close();
+			$conn->close();
+			returnWithError("User already exists");
+			exit;
+		}
+		$stmt->close();
+
+		// Hash the password and insert new user
+		$passwordHash = password_hash($password, PASSWORD_DEFAULT);
+
+		$stmt = $conn->prepare("INSERT INTO Users (firstName, lastName, Login, Password) VALUES (?, ?, ?, ?)");
+		$stmt->bind_param("ssss", $firstName, $lastName, $login, $passwordHash);
+
+		if( $stmt->execute() )
+		{
+			$id = $conn->insert_id;
+			returnWithInfo( $firstName, $lastName, $id );
+		}
+		else
+		{
+			returnWithError("Insert failed");
+		}
+
+		$stmt->close();
+		$conn->close();
+	}
+    
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		echo $obj;
+	}
+    
+	function returnWithError( $err )
+	{
+		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+    
+	function returnWithInfo( $firstName, $lastName, $id )
+	{
+		$retValue = '{"id":' . $id . ',"firstName":"' . $firstName . '","lastName":"' . $lastName . '","error":""}';
+		sendResultInfoAsJson( $retValue );
+	}
+    
+?>

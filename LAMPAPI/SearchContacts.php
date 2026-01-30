8b0f48e78307462d9830a05f070bb1aba02b15c4
Cam <150202195+CameronLewis08@users.noreply.github.com>
2026-01-30 00:15:53 -0500

    Create SearchContacts-API
    Takes unspecific input, it can be a name or phone or email and searches all possible labels in the database for a match it can also be a partial match and is case insensitive. It will either return and error or the array with results.

---
+++ LAMPAPI/SearchContacts.php
@@ -0,0 +1,75 @@
+<?php
+
+	$inData = getRequestInfo();
+	
+	$searchResults = "";
+	$searchCount = 0;
+
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		$stmt = $conn->prepare("SELECT * from Contacts 
+								WHERE LOWER(firstName) LIKE LOWER(?) 
+								OR LOWER(lastName) LIKE LOWER(?) 
+								OR phone LIKE ? 
+								OR email LIKE ?");
+
+
+		$searchTerm = "%" . $inData["search"] . "%";
+		
+		$stmt->bind_param("ssss", $searchTerm, $searchTerm, $searchTerm, $searchTerm);
+		$stmt->execute();
+		
+		$result = $stmt->get_result();
+		
+		$resultsArray = [];
+
+		while($row = $result->fetch_assoc()) 
+		{
+    		$resultsArray[] = [ "firstName" => $row["firstName"],
+        					    "lastName" => $row["lastName"],
+        					    "phone" => $row["phone"],
+        						"email" => $row["email"] ];
+		}
+
+		if (count($resultsArray) === 0) 
+		{
+    		returnWithError("No Records Found");
+		} 
+		else 
+		{
+    		returnWithInfo($resultsArray);
+		}
+		
+		$stmt->close();
+		$conn->close();
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = json_encode(["results" => [], "error" => $err]);
+		sendResultInfoAsJson( $retValue );
+	}
+	
+	function returnWithInfo($resultsArray)
+	{
+    	$retValue = json_encode(["results" => $resultsArray, "error" => ""]);
+    	sendResultInfoAsJson($retValue);
+	}
+	
+?>

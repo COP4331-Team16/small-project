3578ea3c1a86f3fec3f7c0539c1e4524ead1dec4
Damon Bullock <da908718@ucf.edu>
2026-02-16 01:12:29 +0000

    Allow requests from new domain

--- LAMPAPI/Login.php
+++ LAMPAPI/Login.php
@@ -1,4 +1,8 @@
 <?php
+	header("Access-Control-Allow-Origin: http://portcall.cloud");
+	header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+	header("Access-Control-Allow-Headers: Content-Type, Authorization");	
+
 	require_once dirname(__DIR__) . '/vendor/autoload.php';
 
 	use Dotenv\Dotenv;
